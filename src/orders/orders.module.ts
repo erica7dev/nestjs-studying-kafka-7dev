3c6028c740297d1534933306deeef02c3ae9e3ab
erica7monteiro@gmail.com <erikaisa7dev@gmail.com>
2022-07-14 21:02:23 -0300

    arrocha danado

---
+++ src/orders/orders.module.ts
@@ -0,0 +1,38 @@
+import { Order } from './entities/order.entity';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Module } from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { OrdersController } from './orders.controller';
+import { ClientKafka, ClientsModule, Transport } from '@nestjs/microservices';
+
+@Module({
+  imports: [
+    SequelizeModule.forFeature([Order]),
+    ClientsModule.register([
+      {
+        name: 'KAFKA_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            brokers: ['kafka:9092'],
+          },
+          consumer: {
+            groupId: 'my-group-producer',
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [OrdersController],
+  providers: [
+    OrdersService,
+    {
+      provide: 'KAFKA_PRODUCER',
+      useFactory: async (kafkaService: ClientKafka) => {
+        return kafkaService.connect();
+      },
+      inject: ['KAFKA_SERVICE'],
+    },
+  ],
+})
+export class OrdersModule {}
---
+++ src/test/test.controller.ts
@@ -0,0 +1,13 @@
+import { TestService } from './test.service';
+import { Controller, Get, Param } from '@nestjs/common';
+
+@Controller('test')
+export class TestController {
+  constructor(private service: TestService) {}
+
+  @Get(':id') //test/:id
+  acao(@Param('id') id) {
+    console.log(id);
+    return this.service.metodo1();
+  }
+}
